c32b396fb830352abbad212ccb9793015d2dd0e3
Aditya Kakarla <85147239+adityakakarla@users.noreply.github.com>
2025-11-16 09:29:17 -0500

    added error handling

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
+use anyhow::{Context, Result};
 use clap::{Parser, Subcommand};
+use std::error::Error;
 use std::fs::{read_to_string, write};
 
 #[derive(Parser, Debug)]
@@ -13,23 +15,28 @@ enum Commands {
     Run,
 }
 
-fn main() {
+fn main() -> Result<(), Box<dyn Error>> {
     let cli = Cli::parse();
 
     match &cli.command {
-        Commands::Run {} => {
-            let dotenv_file = read_to_string("./.env").unwrap();
-
-            let mut dotenv_example_lines: Vec<String> = Vec::new();
-
-            for line in dotenv_file.trim_ascii().split_ascii_whitespace() {
-                let api_key_name = line.split('=').next().unwrap();
-                dotenv_example_lines.push(format!("{api_key_name}="));
-            }
-            dotenv_example_lines.push(String::new());
-
-            let dotenv_example_file_content = dotenv_example_lines.join("\n");
-            write("./.env.example", dotenv_example_file_content).unwrap();
-        }
+        Commands::Run => run_command(),
     }
+}
+
+fn run_command() -> Result<(), Box<dyn Error>> {
+    let dotenv_file = read_to_string("./.env").context("Failed to read .env file")?;
+
+    let mut dotenv_example_lines: Vec<String> = Vec::new();
+
+    for line in dotenv_file.trim_ascii().split_ascii_whitespace() {
+        let api_key_name = line.split('=').next().unwrap();
+        dotenv_example_lines.push(format!("{api_key_name}="));
+    }
+    dotenv_example_lines.push(String::new());
+
+    let dotenv_example_file_content = dotenv_example_lines.join("\n");
+    write("./.env.example", dotenv_example_file_content)
+        .context("Failed to write .env.example file")?;
+
+    Ok(())
 }
