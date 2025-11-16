fbddf3f4b8fc8cf956afb67963619c9f028cba00
Aditya Kakarla <85147239+adityakakarla@users.noreply.github.com>
2025-11-16 10:01:00 -0500

    restructured code + added tests

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-use anyhow::{Context, Result};
+use anyhow::{Context, Ok, Result};
 use clap::{Parser, Subcommand};
-use std::error::Error;
 use std::fs::{read_to_string, write};
+use std::path::Path;
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
@@ -15,7 +15,7 @@ enum Commands {
     Run,
 }
 
-fn main() -> Result<(), Box<dyn Error>> {
+fn main() -> Result<()> {
     let cli = Cli::parse();
 
     match &cli.command {
@@ -23,20 +23,119 @@ fn main() -> Result<(), Box<dyn Error>> {
     }
 }
 
-fn run_command() -> Result<(), Box<dyn Error>> {
-    let dotenv_file = read_to_string("./.env").context("Failed to read .env file")?;
+fn run_command() -> Result<()> {
+    let dotenv_file = read_dotenv(Path::new("./.env"))?;
+    let dotenv_example_file_content = generate_dotenv_example_file(&dotenv_file)?;
+    write_dotenv_example(Path::new("./.env.example"), &dotenv_example_file_content)?;
 
+    Ok(())
+}
+
+fn read_dotenv(path: &Path) -> Result<String> {
+    let dotenv_content = read_to_string(path).context("Failed to read .env file")?;
+    Ok(dotenv_content)
+}
+
+fn generate_dotenv_example_file(dotenv_file: &String) -> Result<String> {
     let mut dotenv_example_lines: Vec<String> = Vec::new();
 
-    for line in dotenv_file.trim_ascii().split_ascii_whitespace() {
-        let api_key_name = line.split('=').next().unwrap();
+    for line in dotenv_file.trim_ascii().lines() {
+        if line.trim() == "" || line.starts_with("#") {
+            continue;
+        }
+        let api_key_name = line.split('=').next().context("Parsing .env file lines")?;
         dotenv_example_lines.push(format!("{api_key_name}="));
     }
     dotenv_example_lines.push(String::new());
 
-    let dotenv_example_file_content = dotenv_example_lines.join("\n");
-    write("./.env.example", dotenv_example_file_content)
-        .context("Failed to write .env.example file")?;
+    Ok(dotenv_example_lines.join("\n"))
+}
 
+fn write_dotenv_example(path: &Path, dotenv_example_file_content: &String) -> Result<()> {
+    write(path, dotenv_example_file_content).context("Failed to write .env.example file")?;
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn standard_dotenv() -> Result<()> {
+        let dotenv_file_content = r#"OPENAI_API_KEY=ABC123
+GROK_API_KEY=DEF456
+"#
+        .to_string();
+
+        let dotenv_example_file_content = generate_dotenv_example_file(&dotenv_file_content)?;
+
+        assert_eq!(
+            dotenv_example_file_content,
+            r#"OPENAI_API_KEY=
+GROK_API_KEY=
+"#
+        );
+
+        Ok(())
+    }
+
+    #[test]
+    fn without_newline() -> Result<()> {
+        let dotenv_file_content = r#"OPENAI_API_KEY=ABC123
+GROK_API_KEY=DEF456"#
+            .to_string();
+
+        let dotenv_example_file_content = generate_dotenv_example_file(&dotenv_file_content)?;
+
+        assert_eq!(
+            dotenv_example_file_content,
+            r#"OPENAI_API_KEY=
+GROK_API_KEY=
+"#
+        );
+        Ok(())
+    }
+
+    #[test]
+    fn with_blank_lines() -> Result<()> {
+        let dotenv_file_content = r#"
+OPENAI_API_KEY=ABC123
+
+
+
+GROK_API_KEY=DEF456
+
+"#
+        .to_string();
+
+        let dotenv_example_file_content = generate_dotenv_example_file(&dotenv_file_content)?;
+
+        assert_eq!(
+            dotenv_example_file_content,
+            r#"OPENAI_API_KEY=
+GROK_API_KEY=
+"#
+        );
+        Ok(())
+    }
+
+    #[test]
+    fn with_comments() -> Result<()> {
+        let dotenv_file_content = r#"
+OPENAI_API_KEY=ABC123
+# this is pretty cool
+GROK_API_KEY=DEF456
+"#
+        .to_string();
+
+        let dotenv_example_file_content = generate_dotenv_example_file(&dotenv_file_content)?;
+
+        assert_eq!(
+            dotenv_example_file_content,
+            r#"OPENAI_API_KEY=
+GROK_API_KEY=
+"#
+        );
+        Ok(())
+    }
 }
